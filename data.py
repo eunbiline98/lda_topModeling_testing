3e10de6aa21af14c89be4bcbfff889f20abff49f
Ath Thaareq Mahesa <50385294+eunbiline98@users.noreply.github.com>
2022-12-06 17:51:49 +0700

    Intial Commit

---
+++ data.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import nltk
+
+import string
+import re  # regex library
+
+# import word_tokenize & FreqDist from NLTK
+from nltk.tokenize import word_tokenize
+from nltk.probability import FreqDist
+
+# C:\Users\thaar\Documents\work\Project Ariq\LDA\testing 3\lda_test\content\dataBerita.xlsx
+
+dataSB = pd.read_excel(
+    '/Users/thaar/Documents/work/Project Ariq/LDA/testing 3/lda_test/content/dataBerita.xlsx', engine='openpyxl')  # lokasi file
+dataSB.head()
+dataSB['textdata'] = dataSB['textdata'].str.lower()
+
+print('Case Folding Result : \n')
+print(dataSB['textdata'].head(5))
